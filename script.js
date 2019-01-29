be5c2118e13dbdc895769310fa1a76dd7173d3d8
Alex Korshak <alexokorshak@gmail.com>
2019-01-28 16:11:32 -0800

    Add game helper logic

--- script.js
+++ script.js
@@ -1,108 +1,134 @@
+console.log("loaded script.js");
 
-let lineChart = d3.select("#lineChart");
-let height = 800;
-let width = height/2;
-let offsetx = 90;
-let offsety = 10;
-let x = d3.scaleLinear().range([0, width]);
-let y = d3.scaleLinear().range([height, 0]);
+let textField = document.getElementById("textFieldID");
+textField.value = "(6 digit guess + hint)";
 
-let plotArea = lineChart.append("g").attr("transform", "translate(" + offsetx + ", " + offsety + ")");
+let guesses = [];
 
-let xAxis = plotArea.append("g")
-    .attr("transform", "translate(0, " + height + ")")
-    .call(d3.axisBottom(x));
-let yAxis = plotArea.append("g")
-    .call(d3.axisLeft(y));
-
-let dotGroup = plotArea.append("g");
-let xLine = null;
-let xLineText = null;
-let lastDate = 1;
-let valueline = d3.line()
-                .x(function(d) { return x(d.day); })
-                .y(function(d) { return y(d.xp); });
-
-let hovered = function(data) {
-    xLine.datum([{"day":0, "xp":data.xp}, {"day":lastDate, "xp":data.xp}]).attr("d", valueline);
-    xLineText.attr("x", x(0) + 5)
-        .attr("y", y(data.xp) - 3)
-        .text(data.player + ": " + data.xp);
-    console.log(data);
+function updateGuessList() {
+    
+    let guessList = document.getElementById("guessListID");
+    if( guesses.length == 0 ) {
+        guessList.textContent = "Zero guesses so far.";
+        return;
+    }
+    let guessString = "Guesses: ";
+    for( let g of guesses) {
+        guessString += "" + g[0] + g[1] + g[2] + g[3] + ":" + g[4] + g[5] + ", ";
+    }
+    guessList.textContent = guessString;
 }
-let hoverReset = function() {
-    xLine.datum([{"day":0, "xp":200000000}, {"day":lastDate, "xp":200000000}]).attr("d", valueline);
-    xLineText.text("").attr("y", y(200000000));
+function parseInput() {
+    let input = textField.value;
+    textField.value = "";
+    console.log(input);
+
+    console.log(input[0]);
+    console.log(input.length);
+
+    let guess = {};
+    guess[0] = parseInt(input[0]);
+    guess[1] = parseInt(input[1]);
+    guess[2] = parseInt(input[2]);
+    guess[3] = parseInt(input[3]);
+    guess[4] = parseInt(input[4]);
+    guess[5] = parseInt(input[5]);
+    let numCorrect = parseInt(input[4]);
+    let numCorrectSlot = parseInt(input[5]);
+    
+    console.log(numCorrect);
+    console.log(numCorrectSlot);
+    console.log(guess);
+
+    guesses.push(guess)
+    updateGuessList();
+
+}
+function clearGuesses() {
+    guesses = [];
+    updateGuessList();
+    updatePossList("Possibilities: Endless");
 }
 
-
-d3.csv("data.csv").then( data => {
-    console.log(data);
-    let newData = [];
-    let players = [];
-    data.forEach(row => {
-        let name = row.name;
-        if( !players.includes(name) ) {
-            players.push(name);
+function computePossibilities() {
+    console.log("Started compute");
+    let poss = [];
+    let possString = "Possibilities: ";
+    for( let a = 0; a <= 9; a++ ) {
+        for( let b = 0; b <= 9; b++ ) {
+            for( let c = 0; c <= 9; c++ ) {
+                for( let d = 0; d <= 9; d++ ) {
+                    if( a == b || a == c || a == d || b == c || b == d || c == d ) {
+                        continue;
+                    }
+                    //console.log("iter=" + a + b + c + d);
+                    let passes = true;
+                    for( let guess of guesses) {
+                        let numDigit = 0;
+                        let numSlot = 0;
+                        for( let i = 0; i < 4; i++ ) {
+                            if( guess[i] == a ) {
+                                numDigit++;
+                                if( i == 0 ) {
+                                    numSlot++;
+                                }
+                            }
+                            if( guess[i] == b ) {
+                                numDigit++;
+                                if( i == 1 ) {
+                                    numSlot++;
+                                }
+                            }
+                            if( guess[i] == c ) {
+                                numDigit++;
+                                if( i == 2 ) {
+                                    numSlot++;
+                                }
+                            }
+                            if( guess[i] == d ) {
+                                numDigit++;
+                                if( i == 3 ) {
+                                    numSlot++;
+                                }
+                            }
+                        }
+                        if( numDigit == guess[4] && numSlot == guess[5]) {
+                            passes = true;
+                        }
+                        else {
+                            passes = false;
+                            break;
+                        }
+                    }
+                    if( passes == true ) {
+                        let s = "" + a + b + c + d;
+                        poss.push(s);
+                        console.log(s);
+                        possString += s + ", ";
+                    }
+                }
+            }
         }
-        delete row.name;
-        for( let key of Object.keys(row) ) {
-            let obj = { 
-                "player": name,
-                "day": +key,
-                "xp": +row[key]
-            };
-            newData.push(obj)
-        }
-    });
-    console.log(newData);
-    console.log(players);
+    }
+    console.log("finished compute");
+    if( poss.length > 100 ) {
+        updatePossList("There are " + poss.length + " possibilities.");
+    }
+    else {
+        updatePossList(possString);
+    }
 
-    let dates = Object.keys(data[0]).filter( date => {
-        return date != "name";
-    });
-    console.log(dates);
-    lastDate = +dates[dates.length-1];
-    x.domain([dates[0]-1, lastDate]);
-    y.domain([150000000, 200000000]);
-
-    xAxis.call(d3.axisBottom(x));
-    yAxis.call(d3.axisLeft(y));
+    //possList.value = possString;
+    //possList.setAttribute("text", possString);
     
+}
+function updatePossList(possString) {
+    let possList = document.getElementById("possListID");
+    possList.textContent = possString;
+}
 
-    players.forEach( player => {
-        let data = newData.filter(d => {
-            return d.player == player;
-        });
-        let pathSelection = dotGroup.selectAll("path." + player).data([data]);
-        let pathEnterSelection = pathSelection.enter().append("path");
-        pathSelection = pathSelection.merge(pathEnterSelection);
-        pathSelection.attr("d", valueline).attr("stroke-width", 2)
-                        .attr("class", d => d.player)
-                        .attr("fill", "none")
-                        .attr("stroke", "steelblue");
-    });
-
-    xLine = dotGroup.append("path")
-        .attr("stroke", "red")
-        .attr("stroke-width", 2)
-        .datum([{"day":0, "xp":200000000}, {"day":dates[dates.length-1], "xp":200000000}])
-        .attr("d", valueline);
-    
-
-    let circleSelection = dotGroup.selectAll("circle").data(newData);
-    let enterSelection = circleSelection.enter().append("circle");
-    circleSelection.exit().remove();
-    circleSelection = circleSelection.merge(enterSelection);
-    circleSelection.attr("cy", d => y(d.xp))
-                    .attr("cx", d => x(d.day))
-                    .attr("r", 5)
-                    .attr("fill", "gray")
-                    .attr("class", d => d.player)
-                    .on("mouseover", d => hovered(d))
-                    .on("mouseout", d => hoverReset());
-
-    xLineText = dotGroup.append("text").text("asdf");
-    
-
-});
+function enter() { // TODO add option to press keyboard enter for same effect.
+    console.log("pressed enter");
+    parseInput();
+    computePossibilities();
+}
